c2dddaed89610b189457e2beb0db28576f777a8c
Coutre <noahmauro@rogers.com>
2023-10-04 18:35:01 -0400

    Assingment 1 w/comments

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
+// index.js, Noah Mauro, 301191967, 10-04-23
+//requiring various modules
 const express = require("express");
 const path = require("path");
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
 
+//setting view engine and directories
 app.set('view engine', 'ejs');
 app.set('views', "Views");
 app.locals.partialsDir = path.join(__dirname, 'Views','partials');
 app.use(express.static(path.join(__dirname, 'Public')));
 
+//Handling Get requests
 app.get("/", (req, res) => {
     res.render("index.ejs"); 
     console.log("I recieved a get Request");
@@ -33,4 +37,5 @@ app.get("/Services", (req, res) => {
     console.log("I recieved a get Request");
 });
 
+//start server on port 3000
 app.listen(3000, () => {console.log("Server is running on port 3000")});
